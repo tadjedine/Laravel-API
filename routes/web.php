7451a9e738f63c228a9b340299262e66feeffb4c
tadjedine <tadjallali@gmail.com>
2026-03-27 23:04:59 +0100

    established connection with prestashop's database, added reliese model generator package

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Laravel' => app()];
 });
+
+Route::get('/test-db', function () {
+
+    if(DB::connection()->getPdo()) return "Databse success";
+     
+});
+
 
