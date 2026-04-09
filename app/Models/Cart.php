df2ee734699985e058cd3893520e72727802d099
tadjedine <tadjallali@gmail.com>
2026-04-09 16:46:21 +0100

    Added necessary order relationships in the relevant models

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,8 +6,6 @@
 
 namespace App\Models;
 
-use App\Models\CartProduct;
-use App\Models\Customer;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,7 +13,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class PsCart
- * 
+ *
  * @property int $id_cart
  * @property int $id_shop_group
  * @property int $id_shop
@@ -36,57 +34,56 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property Carbon $date_add
  * @property Carbon $date_upd
  * @property string|null $checkout_session_data
- *
- * @package App\Models
  */
 class Cart extends Model
 {
-	protected $table = 'ps_cart';
-	protected $primaryKey = 'id_cart';
-	public $timestamps = false;
+    protected $table = 'ps_cart';
 
-	protected $casts = [
-		'id_shop_group' => 'int',
-		'id_shop' => 'int',
-		'id_carrier' => 'int',
-		'id_lang' => 'int',
-		'id_address_delivery' => 'int',
-		'id_address_invoice' => 'int',
-		'id_currency' => 'int',
-		'id_customer' => 'int',
-		'id_guest' => 'int',
-		'recyclable' => 'int',
-		'gift' => 'int',
-		'mobile_theme' => 'bool',
-		'allow_seperated_package' => 'int',
-		'date_add' => 'datetime',
-		'date_upd' => 'datetime'
-	];
+    protected $primaryKey = 'id_cart';
 
-	protected $fillable = [
-		'id_shop_group',
-		'id_shop',
-		'id_carrier',
-		'delivery_option',
-		'id_lang',
-		'id_address_delivery',
-		'id_address_invoice',
-		'id_currency',
-		'id_customer',
-		'id_guest',
-		'secure_key',
-		'recyclable',
-		'gift',
-		'gift_message',
-		'mobile_theme',
-		'allow_seperated_package',
-		'date_add',
-		'date_upd',
-		'checkout_session_data'
-	];
+    public $timestamps = false;
 
+    protected $casts = [
+        'id_shop_group' => 'int',
+        'id_shop' => 'int',
+        'id_carrier' => 'int',
+        'id_lang' => 'int',
+        'id_address_delivery' => 'int',
+        'id_address_invoice' => 'int',
+        'id_currency' => 'int',
+        'id_customer' => 'int',
+        'id_guest' => 'int',
+        'recyclable' => 'int',
+        'gift' => 'int',
+        'mobile_theme' => 'bool',
+        'allow_seperated_package' => 'int',
+        'date_add' => 'datetime',
+        'date_upd' => 'datetime',
+    ];
 
-	// Relationships
+    protected $fillable = [
+        'id_shop_group',
+        'id_shop',
+        'id_carrier',
+        'delivery_option',
+        'id_lang',
+        'id_address_delivery',
+        'id_address_invoice',
+        'id_currency',
+        'id_customer',
+        'id_guest',
+        'secure_key',
+        'recyclable',
+        'gift',
+        'gift_message',
+        'mobile_theme',
+        'allow_seperated_package',
+        'date_add',
+        'date_upd',
+        'checkout_session_data',
+    ];
+
+    // Relationships
     public function customer(): BelongsTo
     {
         return $this->belongsTo(Customer::class, 'id_customer', 'id_customer');
@@ -97,9 +94,19 @@ class Cart extends Model
         return $this->hasMany(CartProduct::class, 'id_cart', 'id_cart');
     }
 
+    public function items(): HasMany
+    {
+        return $this->products();
+    }
+
+    public function order(): HasMany
+    {
+        return $this->hasMany(Order::class, 'id_cart', 'id_cart');
+    }
+
     // public function cartRules(): HasMany
     // {
     //     return $this->hasMany(CartCartRule::class, 'id_cart', 'id_cart'); // if you need promo codes
     // }
-	
+
 }
--- app/Models/CartProduct.php
+++ app/Models/CartProduct.php
@@ -6,15 +6,13 @@
 
 namespace App\Models;
 
-use App\Models\Cart;
-use App\Models\ProductAttribute;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Class PsCartProduct
- * 
+ *
  * @property int $id_cart
  * @property int $id_product
  * @property int $id_address_delivery
@@ -23,38 +21,43 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property int $id_customization
  * @property int $quantity
  * @property Carbon $date_add
- *
- * @package App\Models
  */
 class CartProduct extends Model
 {
-	protected $table = 'ps_cart_product';
-	public $incrementing = false;
-	public $timestamps = false;
+    protected $table = 'ps_cart_product';
 
-	protected $casts = [
-		'id_cart' => 'int',
-		'id_product' => 'int',
-		'id_address_delivery' => 'int',
-		'id_shop' => 'int',
-		'id_product_attribute' => 'int',
-		'id_customization' => 'int',
-		'quantity' => 'int',
-		'date_add' => 'datetime'
-	];
+    public $incrementing = false;
 
-	protected $fillable = [
-		'id_shop',
-		'quantity',
-		'date_add'
-	];
+    public $timestamps = false;
 
-	public function cart(): BelongsTo
+    protected $casts = [
+        'id_cart' => 'int',
+        'id_product' => 'int',
+        'id_address_delivery' => 'int',
+        'id_shop' => 'int',
+        'id_product_attribute' => 'int',
+        'id_customization' => 'int',
+        'quantity' => 'int',
+        'date_add' => 'datetime',
+    ];
+
+    protected $fillable = [
+        'id_cart',
+        'id_product',
+        'id_address_delivery',
+        'id_shop',
+        'id_product_attribute',
+        'id_customization',
+        'quantity',
+        'date_add',
+    ];
+
+    public function cart(): BelongsTo
     {
         return $this->belongsTo(Cart::class, 'id_cart', 'id_cart');
     }
 
-	public function product(): BelongsTo
+    public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class, 'id_product', 'id_product');
     }
@@ -64,5 +67,8 @@ class CartProduct extends Model
         return $this->belongsTo(ProductAttribute::class, 'id_product_attribute', 'id_product_attribute');
     }
 
-	
+    public function productAttribute(): BelongsTo
+    {
+        return $this->combination();
+    }
 }
